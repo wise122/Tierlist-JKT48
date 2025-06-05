f520a70a1557d6dfda36ccfeae5738cf0199e329
MrcellSbst <marcellsubastian123@gmail.com>
2025-06-05 18:40:59 +0700

    agaINNN

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -319,6 +319,17 @@ const Calculator = () => {
                             maxWidth: '600px'
                         }
                     }}
+                    sx={{
+                        '& .MuiDialog-container': {
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0
+                        },
+                        position: 'fixed'
+                    }}
+                    keepMounted
                 >
                     <DialogTitle sx={{ 
                         borderBottom: '1px solid rgba(255, 255, 255, 0.12)',
